a29fa538db248e33f3892ef1ff22d079e70437f0
RJaniec <rafael.janiec@outlook.com>
2022-09-01 18:24:54 +0200

    1.01

---
+++ cmake-build-debug/CMakeFiles/offlineTestPartRepo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/offlineTestPartRepo.dir/main.cpp.o"
+  "CMakeFiles/offlineTestPartRepo.dir/main.cpp.o.d"
+  "offlineTestPartRepo"
+  "offlineTestPartRepo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/offlineTestPartRepo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
